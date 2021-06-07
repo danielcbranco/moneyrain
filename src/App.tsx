f45089c14c4ea88369fbbaf7ef9d18a523818583
danielcbranco <daniel-cb1@hotmail.com>
2021-06-06 21:48:02 -0300

    ultimo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
         <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
         <Dashboard />
@@ -31,7 +32,7 @@ export function App() {
         
 
         <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
